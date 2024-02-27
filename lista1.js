405c073a1631415de36324e96c81ba63bc67ce18
Sabolinha <Arthurquintino2304@gmail.com>
2024-02-27 19:09:07 -0300

    Um dos meus primeiros projetos js :)

---
+++ lista1.js
@@ -0,0 +1,48 @@
+
+
+/*
+01 - Declare uma const "myBirthYear", que recebe o ano do seu nascimento, com 4  
+dígitos. Ex.: 2001.
+*/
+
+const myBirthYear = 2008
+
+/*
+02 - Declare uma const "currentYear", que recebe o ano atual, com 4 dígitos.
+*/
+
+const currentYear = 2024
+
+/*
+03 - Exiba as const "myBirthYear" e "currentYear", lado a lado, no console do browser.
+*/
+
+console.log("eu nasci em " + myBirthYear + " o ano atual é " + currentYear)
+
+/*
+04 - Experimente.
+- Declare uma const "myAge", que recebe o ano atual subtraído pelo ano do seu  
+nascimento. Mas sem digitar os números manualmente.
+Você pode utilizar o sinal de menos (-) entre os dois valores e verificar o que
+acontece. 
+- Exiba a "myAge" no console.
+*/
+
+const myAge = currentYear - myBirthYear
+console.log("eu tenho " + myAge + " anos")
+
+/*
+05 - Antes do console.log abaixo, declare uma const "ten", está const deverá receber o número  
+10 e:
+- Descomente o console.log abaixo e;
+- Substitua a palavra "valor1" pelo ano atual somado à 10;
+- Substitua a palavra "valor2" pela sua idade somada à 10;
+- Os números não devem ser digitados manualmente;
+- Observe o resultado no console do browser.
+Não se assuste com as crases e os cifrões. Conversaremos mais sobre operações  
+matemáticas e strings (textos) em breve =)
+*/
+
+const ten = 10
+console.log(`Em ${currentYear + ten}, estarei com ${myAge + ten} anos.`)
+
